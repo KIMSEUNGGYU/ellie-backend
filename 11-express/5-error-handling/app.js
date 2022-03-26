c4e60dd53501a64cfeb6693657b648ca0502552c
kimseunggyu <dev.seunggyu@gmail.com>
2022-03-26 19:42:30 +0900

    [11.9] 비동기 에러 처리 하는 방법 - 최신버전

--- 11-express/5-error-handling/app.js
+++ 11-express/5-error-handling/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import fs from "fs";
 import fsAsync from "fs/promises";
 
+// require("express-async-errors"); // Common JS
+import {} from "express-async-errors";
+
 const app = express();
 
 app.use(express.json());
@@ -35,27 +38,18 @@ app.get("/file2", (req, res) => {
   // Promise 도 마찬가지로 callback 함수를 등록하지 않지만, 비슷한 형태인 then, catch 를 수행
   // promise 코드를 호출하더라도 에러를 내부적으로 처리하기 때문에 외부에서 에러 발생하는지 확인 불가 - try-catch 사용 불가
   // promise 에서 에러 처리할 경우 catch 로 처리해야함
-  fsAsync
-    .readFile("/file2.txt") //
-    .catch((error) => {
-      res.sendStatus(404);
-    });
+  // fsAsync
+  //   .readFile("/file2.txt") //
+  //   .catch((error) => {
+  //     res.sendStatus(404);
+  //   });
+
+  return fsAsync.readFile("/file2.txt");
 });
 
 // async 예제
 app.get("/file3", async (req, res) => {
-  try {
-    const data = await fsAsync.readFile("/file2.txt");
-    // 해당 부분은 동기적으로 동작함.
-    // 하지만 error 미들웨어까지 도달하지 못함.
-    // 왜냐하면 async-await 패턴은 결국 promise 를 반환하기 때문!
-    // => 48 라인에서 에러가 발생해도 moddleware error handler 까지 도달하지 못함
-
-    // 단, async 패턴은 promise 를 동기적으로 사용할 수 있게 했으므로
-    // try-catch 문으로 에러를 잡을 수 있음
-  } catch {
-    res.sendStatus(404);
-  }
+  const data = await fsAsync.readFile("/file2.txt");
 });
 
 // 버전 5 이하에서는: require('express-async-errors');
@@ -72,4 +66,11 @@ app.listen(8080);
  * 동기적인 코드인 경우 middleware error handler 까지 떨어지지만
  * 비동기적인 처리(코드)는 (promsie, async-await) 외부에서 에러를 감지할 수 있는 방법이 없어
  * middleware error handler 가 있더라도 알 수 없음
+ */
+
+/**
+ * Express.js 5.0.0-alpha.7 에서 에러 발생시, 미들웨어 에러 핸들러를 호출할 수 있도록 할 예정
+ *
+ * Express 4점대 에서는 express-async-erros 를 import 하여 사용 <- 자동으로 미들웨어 에러 핸들로 호출 함(try-catch 없어도?)
+ * 단, promise 를 return 하는 경우에만 감지해서 동작
  */
