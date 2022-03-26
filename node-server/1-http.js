1fc3c607569958a38d0dd99aa52b4e8b10884a64
kimseunggyu <dev.seunggyu@gmail.com>
2022-03-26 13:27:45 +0900

    [9-3] HTML 콘텐츠 보내주기

--- node-server/1-http.js
+++ node-server/1-http.js
@@ -1,19 +1,24 @@
 const http = require("http");
-// const http2 = require("http2"); // 개발할 때는 http 로 하기, spec 은 같기 때문에 배포할 때 http2 로 배포하면됨.?
-
-// console.log(http.STATUS_CODES);
-// console.log(http.METHODS);
+const fs = require("fs");
 
 // 서버 생성
 const server = http.createServer((req, res) => {
   console.log("incomming...");
-  console.log(req.headers);
-  console.log(req.httpVersion);
-  console.log(req.method);
-  console.log(req.url);
+  //   console.log(req.headers);
+  //   console.log(req.httpVersion);
+  //   console.log(req.method);
+  //   console.log(req.url);
 
-  res.write("Welcome!");
-  res.end();
+  const url = req.url;
+  res.setHeader("Content-type", "text/html");
+
+  if (url === "/") {
+    fs.createReadStream("./html/index.html").pipe(res);
+  } else if (url === "/courses") {
+    fs.createReadStream("./html/courses.html").pipe(res);
+  } else {
+    fs.createReadStream("./html/not-found.html").pipe(res);
+  }
 });
 
 // listen 을 호출해야 server 수행
