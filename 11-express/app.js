e1764e273a27205771fbdbb8f0be4f920078488f
kimseunggyu <dev.seunggyu@gmail.com>
2022-03-26 15:24:15 +0900

    [11.4] 요청에 대해서

--- 11-express/app.js
+++ 11-express/app.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res, next) => {
+  //   console.log(req.path);
+  //   console.log(req.headers);
+  console.log(req.params);
+  console.log(req.query);
   console.log("gyu");
 });
 
