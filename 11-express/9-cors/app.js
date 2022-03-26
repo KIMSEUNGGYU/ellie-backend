b9c8d074b41986a2785d2e56534f4989bc3ad7e0
kimseunggyu <dev.seunggyu@gmail.com>
2022-03-26 20:12:58 +0900

    [11.12] cors 란? 개념과 처리 방법

---
+++ 11-express/9-cors/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// before - cors 에러 처리하기 위해 header 추가
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "OPTIONS, GET, POST, PUT, DELETE"
+//   );
+//   next();
+// });
+
+// after - cors 패키지 사용
+app.use(
+  cors({
+    origin: ["http://127.0.0.1:5500"],
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
+app.listen(8080);
