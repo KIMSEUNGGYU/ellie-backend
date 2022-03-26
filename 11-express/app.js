e50a209936ca39eb54f115a10109e92d1c2c8eb5
kimseunggyu <dev.seunggyu@gmail.com>
2022-03-26 15:26:58 +0900

    [11.5] 응답에 대해서

--- 11-express/app.js
+++ 11-express/app.js
@@ -7,7 +7,13 @@ app.get("/", (req, res, next) => {
   //   console.log(req.headers);
   console.log(req.params);
   console.log(req.query);
-  console.log("gyu");
+
+  //   res.send("hi!!");
+  //   res.json({ name: "gyu" });
+  //   res.sendStatus(400);
+
+  res.setHeader("key", "value");
+  res.status(201).send("created");
 });
 
 app.listen(8080);
