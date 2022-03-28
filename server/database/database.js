0a8cc6a52a8f457dc3e9b19428916e189c096f87
kimseunggyu <dev.seunggyu@gmail.com>
2022-03-28 14:43:10 +0900

    [23.4] Mongoose 드위터에 적용하기- Auth 💡

--- server/database/database.js
+++ server/database/database.js
@@ -5,11 +5,17 @@ export async function connectDB() {
   return Moogoose.connect(config.db.host);
 }
 
+// _id -> id
+export function useVirtualId(schema) {
+  schema.virtual("id").get(function () {
+    return this._id.toString();
+  });
+  schema.set("toJSON", { virtuals: true });
+  schema.set("toObject", { virtuals: true });
+}
+
 // TODO(GYU): Delete blow
 let db;
-export function getUsers() {
-  return db.collection("users");
-}
 
 export function getTweets() {
   return db.collection("tweets");
