355c0b0a5a16e03169e575973e0296a554559c5b
kimseunggyu <dev.seunggyu@gmail.com>
2022-03-28 12:51:14 +0900

    22.6 드위터 트윗 관련 MongoDB 사용하기 ❓ (NoSQL 포인트 정리)

--- server/data/tweet.js
+++ server/data/tweet.js
@@ -1,17 +1,34 @@
-import * as userRepository from './auth.js';
+import * as userRepository from "./auth.js";
+
+// NOSQL (정보의 중복 > 관계)
+// 프로필 DB
+// 사용자의 문서 DB: 서버1, 서버2, 서버3
+// 서로 관계가 없음. 수평적으로 확장 쉬움
+// 관계형 조인쿼리의 성능이 좋지 않음!
+// 모든 테이블 간의 관계가 있으면 SQL 사용 고려
+// => 특정 테이블의 관계가 있는 경우 정보의 중복성을 관계보다 더 선호함
+// => A 컬렉션이 B컬렉션의 정보를 가지고 있다면, 관계를 맺지 말고 A 컬렉션에서 B 컬렉션의 정보를 갖고 있는 것을 선호
+// => 왜? 중복을 가지고 있는 것이 관계를 갖는 것보다 성능적으로 확장이 쉬움?
+
+// SQL: 관계형
+// 조인쿼리의 성능이 좋음
 
 let tweets = [
   {
-    id: '1',
-    text: '드림코더분들 화이팅!',
+    id: "1",
+    text: "드림코더분들 화이팅!",
     createdAt: new Date().toString(),
-    userId: '1',
+    userId: "1",
+    userName: "gyu",
+    url: "...",
   },
   {
-    id: '2',
-    text: '안뇽!',
+    id: "2",
+    text: "안뇽!",
     createdAt: new Date().toString(),
-    userId: '1',
+    userId: "1",
+    userName: "gyu",
+    url: "...",
   },
 ];
 
