7eb046566da9f3dc421bc0a103c8f916f6243109
kimseunggyu <dev.seunggyu@gmail.com>
2022-03-28 12:34:03 +0900

    [22.5] 드위터 Auth 관련 MongoDB 사용하기 💡

--- server/database/database.js
+++ server/database/database.js
@@ -2,7 +2,20 @@ import MongoDb from "mongodb";
 
 import { config } from "../config.js";
 
+// 해당 모듈 안에서만 사용할 수 있는 db 변수
+let db;
 export async function connectDB() {
   return MongoDb.MongoClient.connect(config.db.host) //
-    .then((client) => client.db());
+    .then((client) => {
+      db = client.db();
+    });
+}
+
+// 컬렉션을 줄 수 있는 함수 모듈화
+export function getUsers() {
+  return db.collection("users");
+}
+
+export function getTweets() {
+  return db.collection("tweets");
 }
